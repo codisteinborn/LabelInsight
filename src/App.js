a52e1331457df9fc519607a033351926e5e4de7b
Codi <codisteinborn@gmail.com>
2018-05-21 11:55:20 -0500

    thumbnails displayed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,68 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.slice(0,25)
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  onThumbnailClick (){
+
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Welcome to React</h1>
+    //     </header>
+    //     <p className="App-intro">
+    //       To get started, edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //   </div>
+    // );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    }
+    else if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    else {
+      return (
+        // <ul>
+        <div>
+          {items.map(item => (
+            <img key={item.id} src={item.thumbnailUrl} alt={item.id}/>
+          ))}
+          </div>
+        // </ul>
+      );
+    }
   }
 }
 
